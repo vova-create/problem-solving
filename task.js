757293992bf0ff6a9befd140085dbdae4e68fb4b
oleniev_vova <oleniev9@gmail.com>
2022-04-11 21:27:19 +0300

    add the date-time function changed

--- task.js
+++ task.js
@@ -5,26 +5,62 @@ let rl = readline.createInterface({
     output: process.stdout
 });
 
-function test() {
-    rl.question("Would you like to know time or date? ", (string) => {
-
-        let today = new Date();
-        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-        if (string === 'date') {
-            console.log(date)
-            test()
-        } else if (string === 'time') {
-            console.log(time)
-            test()
-        } else if (string === 'exit') {
-            rl.close()
-        } else {
-        console.log('invalid text')
-        test()
-        }
-    });
+//  function test() {
+//     rl.question("Would you like to know time or date? ", (string) => {
+//
+//         let today = new Date();
+//         let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+//         let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//
+//         if (string === 'date') {
+//             console.log(date)
+//             test()
+//         } else if (string === 'time') {
+//             console.log(time)
+//             test()
+//         } else if (string === 'exit') {
+//             rl.close()
+//         } else {
+//         console.log('invalid text')
+//         test()
+//         }
+//     });
+// }
+//
+// test()
+function askQuestion(question) {
+    return new Promise(((resolve) => {
+        rl.question(question, resolve)
+    }))
 }
+const question = "Would you like to know time or date? ";
 
-test()
+function timeProgram(question) {
+    askQuestion(question)
+        .then((string) => {
+            let today = new Date();
+            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+            switch (string) {
+                case 'date':
+                    process.stdout.write(date);
+                    timeProgram(question);
+                    break;
+                case 'time':
+                    process.stdout.write(time);
+                    timeProgram(question);
+                    break;
+                case 'exit':
+                    rl.close();
+                    break;
+                default:
+                    process.stdout.write('invalid text')
+                    timeProgram(question);
+
+
+            }
+        })
+
+}
+timeProgram(question);
